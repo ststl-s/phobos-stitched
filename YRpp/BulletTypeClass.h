033702b141763e60153050e480c396f2d45cfba4
ststl <alskdjfh2018@qq.com>
2022-09-06 13:23:56 +0800

    修正YRpp中的错误

--- YRpp/BulletTypeClass.h
+++ YRpp/BulletTypeClass.h
@@ -27,46 +27,50 @@ public:
 	//static BulletTypeClass* __fastcall FindOrAllocate(const char* pID) JMP_STD(0x46C790);
 
 	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) JMP_STD(0x46C750);
+	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) override JMP_STD(0x46C750);
 
 	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) JMP_STD(0x46C6A0);
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) JMP_STD(0x46C730);
+	virtual HRESULT __stdcall Load(IStream* pStm) override JMP_STD(0x46C6A0);
+	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) override JMP_STD(0x46C730);
 
 	//Destructor
-	virtual ~BulletTypeClass() JMP_THIS(0x46C890);
+	virtual ~BulletTypeClass() override JMP_THIS(0x46C890);
 
 	//AbstractClass
-	virtual void PointerExpired(AbstractType* pAbstract, bool removed) JMP_THIS(0x46C820);
-	virtual AbstractType WhatAmI() const { return AbstractType::BulletType; }
-	virtual int Size() const { return 0x2F8; }
-	virtual void CalculateChecksum(Checksummer& checksum) const JMP_THIS(0x46C560);
+	virtual void PointerExpired(AbstractClass* pAbstract, bool removed) override JMP_THIS(0x46C820);
+	virtual AbstractType WhatAmI() const override { return AbstractType::BulletType; }
+	virtual int Size() const override { return 0x2F8; }
+	virtual void CalculateChecksum(Checksummer& checksum) const override JMP_THIS(0x46C560);
 
 	//AbstractTypeClass
-	virtual bool LoadFromINI(CCINIClass* pINI) JMP_THIS(0x46BEE0);
+	virtual bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x46BEE0);
 
 	//ObjectTypeClass
-	virtual CoordStruct* vt_entry_6C(CoordStruct* pDest, CoordStruct* pSrc) JMP_THIS(0x46C4F0);
-	virtual bool SpawnAtMapCoords(CellStruct* pMapCoords, HouseClass* pOwner) { return nullptr; } //return nullptr directly again
-	virtual ObjectClass* CreateObject(HouseClass* owner) { return nullptr; } //again
+	virtual CoordStruct* vt_entry_6C(CoordStruct* pDest, CoordStruct* pSrc) const override JMP_THIS(0x46C4F0);
+	virtual bool SpawnAtMapCoords(CellStruct* pMapCoords, HouseClass* pOwner) override { return false; } //return false directly again
+	virtual ObjectClass* CreateObject(HouseClass* owner) override { return nullptr; } //again
 
-	bool Rotates() const {
+	bool Rotates() const
+	{
 		return !this->NoRotate;
 	}
 
-	void SetScaledSpawnDelay(int delay) {
+	void SetScaledSpawnDelay(int delay)
+	{
 		 JMP_THIS(0x46C840);
 		//this->ScaledSpawnDelay = delay;
 	}
 
-	BulletClass* __fastcall CreateBullet(
+	BulletClass* __fastcall CreateBullet
+	(
 		AbstractClass* Target,
 		TechnoClass* Owner,
 		int Damage,
 		WarheadTypeClass *WH,
 		int Speed,
-		bool Bright)
-		{ JMP_STD(0x46B050); }
+		bool Bright
+	)
+	{ JMP_STD(0x46B050); }
 
 	//Constructor
 	BulletTypeClass(const char* pID) noexcept
