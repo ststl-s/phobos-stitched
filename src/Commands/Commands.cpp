066f13cd8c9fa40b50ead65e7ce8cf5c02e56e93
secsome <302702960@qq.com>
2022-09-27 10:19:06 +0800

    Split commands implementation

--- src/Commands/Commands.cpp
+++ src/Commands/Commands.cpp
@@ -1,6 +1,5 @@
 #include "Commands.h"
 
-#include "Dummy.h"
 #include "ObjectInfo.h"
 #include "NextIdleHarvester.h"
 #include "QuickSave.h"
@@ -17,9 +16,7 @@ int RepeatLastCombatCommandClass::LastBuildingID = -1;
 DEFINE_HOOK(0x533066, CommandClassCallback_Register, 0x6)
 {
 	// Load it after Ares'
-	// Debug::Log("[Phobos] CommandClassCallback_Register Called!\n");
-
-	//MakeCommand<DummyCommandClass>();
+	
 	MakeCommand<ObjectInfoCommandClass>();
 	MakeCommand<NextIdleHarvesterCommandClass>();
 	MakeCommand<QuickSaveCommandClass>();
