307ebad56eee2bc50047b0c92dae99c8d2476a7e
飞星如梦 <851903106@qq.com>
2023-07-03 18:24:59 +0800

    修一下战役重开崩溃的问题

--- src/Ext/Sidebar/Hooks.cpp
+++ src/Ext/Sidebar/Hooks.cpp
@@ -43,8 +43,11 @@ DEFINE_HOOK(0x6A6EB1, SidebarClass_DrawIt_ProducingProgress, 0x6)
 {
 	if (Phobos::UI::ShowProducingProgress)
 	{
-		auto pPlayer = HouseClass::CurrentPlayer();
-		auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(HouseClass::CurrentPlayer->SideIndex));
+		HouseClass* pPlayer = HouseClass::CurrentPlayer;
+		if (!pPlayer)
+			return 0;
+
+		auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(pPlayer->SideIndex));
 		int XOffset = pSideExt->Sidebar_GDIPositions ? 29 : 32;
 		int XBase = (pSideExt->Sidebar_GDIPositions ? 26 : 20) + pSideExt->Sidebar_ProducingProgress_Offset.Get().X;
 		int YBase = 197 + pSideExt->Sidebar_ProducingProgress_Offset.Get().Y;
@@ -98,9 +101,13 @@ DEFINE_HOOK(0x6A9E2D, SidebarClass_DrawText_Ready, 0x7)
 	LEA_STACK(TextPrintType*, Flag, STACK_OFFSET(0x4A4, -0x490));
 	LEA_STACK(COLORREF*, ForeColor, STACK_OFFSET(0x4A4, -0x498));
 
+	HouseClass* pPlayer = HouseClass::CurrentPlayer;
+	if (!pPlayer)
+		return 0;
+
 	*Flag |= static_cast<TextPrintType>(RulesExt::Global()->TextType_Ready.Get(TextPrintType::Background));
 
-	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(HouseClass::CurrentPlayer->SideIndex));
+	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(pPlayer->SideIndex));
 	auto pRulesExt = RulesExt::Global();
 
 	if (pSideExt->TextColor_Ready.isset())
@@ -116,9 +123,13 @@ DEFINE_HOOK(0x6A9F74, SidebarClass_DrawText_Hold_Multiple, 0x7)
 	LEA_STACK(TextPrintType*, Flag, STACK_OFFSET(0x4A4, -0x490));
 	LEA_STACK(COLORREF*, ForeColor, STACK_OFFSET(0x4A4, -0x498));
 
+	HouseClass* pPlayer = HouseClass::CurrentPlayer;
+	if (!pPlayer)
+		return 0;
+
 	*Flag |= static_cast<TextPrintType>(RulesExt::Global()->TextType_Hold_Multiple.Get(TextPrintType::Background));
 
-	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(HouseClass::CurrentPlayer->SideIndex));
+	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(pPlayer->SideIndex));
 	auto pRulesExt = RulesExt::Global();
 
 	if (pSideExt->TextColor_Hold_Multiple.isset())
@@ -134,9 +145,13 @@ DEFINE_HOOK(0x6AA00B, SidebarClass_DrawText_Hold_Singular, 0x7)
 	LEA_STACK(TextPrintType*, Flag, STACK_OFFSET(0x4A4, -0x490));
 	LEA_STACK(COLORREF*, ForeColor, STACK_OFFSET(0x4A4, -0x498));
 
+	HouseClass* pPlayer = HouseClass::CurrentPlayer;
+	if (!pPlayer)
+		return 0;
+
 	*Flag |= static_cast<TextPrintType>(RulesExt::Global()->TextType_Hold_Singular.Get(TextPrintType::Background));
 
-	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(HouseClass::CurrentPlayer->SideIndex));
+	auto pSideExt = SideExt::ExtMap.Find(SideClass::Array->GetItem(pPlayer->SideIndex));
 	auto pRulesExt = RulesExt::Global();
 
 	if (pSideExt->TextColor_Hold_Singular.isset())
@@ -192,6 +207,8 @@ DEFINE_HOOK(0x6AB64F, SidebarClass_ClickedAction, 0x6)
 	GET(TechnoTypeClass*, pItem, EAX);
 
 	HouseClass* pHouse = HouseClass::CurrentPlayer;
+	if (!pHouse)
+		return 0;
 
 	if (pItem)
 	{
