e1214ee5d247690f243974c4288542f8763c0416
ststl <alskdjfj2018@qq.com>
2023-10-08 17:13:35 +0800

    合并钩子

--- src/Ext/SWType/Hooks.cpp
+++ src/Ext/SWType/Hooks.cpp
@@ -15,19 +15,6 @@
 #include <Utilities/GeneralUtils.h>
 #include <Utilities/Macro.h>
 
-DEFINE_HOOK(0x6CC390, SuperClass_Launch, 0x6)
-{
-	GET(SuperClass* const, pSuper, ECX);
-	GET_STACK(CellStruct const* const, pCell, 0x4);
-	GET_STACK(bool const, isPlayer, 0x8);
-
-	Debug::Log("[Phobos Launch] %s\n", pSuper->Type->get_ID());
-
-	auto const handled = SWTypeExt::Activate(pSuper, *pCell, isPlayer);
-
-	return handled ? 0x6CDE40 : 0;
-}
-
 //Ares hooked at 0x6CC390 and jumped to 0x6CDE40
 // If a super is not handled by Ares however, we do it at the original entry point
 DEFINE_HOOK_AGAIN(0x6CC390, SuperClass_Place_FireExt, 0x6)
@@ -35,20 +22,30 @@ DEFINE_HOOK(0x6CDE40, SuperClass_Place_FireExt, 0x4)
 {
 	GET(SuperClass* const, pSuper, ECX);
 	GET_STACK(CellStruct const* const, pCell, 0x4);
-	//GET_STACK(bool const, isPlayer, 0x8);
+	GET_STACK(bool const, isPlayer, 0x8);
+
+	if (R->Origin() == 0x6CC390)
+	{
+		Debug::Log("[Phobos Launch] %s\n", pSuper->Type->get_ID());
+
+		auto const handled = SWTypeExt::Activate(pSuper, *pCell, isPlayer);
+
+		if (handled)
+			return 0x64DE40;
+	}
 
 	SWTypeExt::FireSuperWeaponExt(pSuper, *pCell);
 
 	return 0;
 }
 
-DEFINE_HOOK(0x50B1CA, SuperClass_After_Update, 0x6)
-{
-	// PhobosGlobal::Global()->CheckSuperQueued();
-	// PhobosGlobal::Global()->CheckFallUnitQueued();
-
-	return 0;
-}
+//DEFINE_HOOK(0x50B1CA, SuperClass_After_Update, 0x6)
+//{
+//	// PhobosGlobal::Global()->CheckSuperQueued();
+//	// PhobosGlobal::Global()->CheckFallUnitQueued();
+//
+//	return 0;
+//}
 
 DEFINE_HOOK(0x6CB5D2, SuperClass_Grant_AddToShowTimer, 0x9)
 {
